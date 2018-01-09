03fea9f87c73b2adcb77015066aa3643c752e341
Unknown <stobenetti@gmail.com>
2018-01-09 14:37:26 -0200

    Controlador

---
+++ controllers/System_Update.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Created by Raquel Baldavira on PhpStorm.
+ * DS Aplicativos for dlg
+ * Date: 08/01/2018
+ * Time: 15:44
+ */
+
+class System_Update extends MY_Controller {
+
+    public function __construct()
+    {
+        parent::__construct();
+        $this->load->model('system_update/FileReader', 'reader');
+    }
+
+    public function index() {
+        $data['info'] = $this->reader->send();
+        $html = $this->load->view('system_update/index', $data, true);
+        $this->show_public($html);
+    }
+
+
+}
